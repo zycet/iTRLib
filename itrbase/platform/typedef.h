cd660f927819e1a0fdf6e11838f52aebb1a57367
张以成 <zycet@126.com>
2013-09-03 15:17:49 +0800

    改目录commom名称为platform

---
+++ itrbase/platform/typedef.h
@@ -0,0 +1,15 @@
+#ifndef ITRBASE_PLATFORM_TYPEDEF_H_
+#define ITRBASE_PLATFORM_TYPEDEF_H_
+
+typedef	unsigned char U8;
+typedef	signed char S8;
+typedef	unsigned int U32;
+typedef	signed int S32;
+typedef	float F32;
+typedef	double F64;
+typedef	unsigned short U16;
+typedef	signed short S16;
+typedef	unsigned long long U64;
+typedef	signed long long S64;
+
+#endif
