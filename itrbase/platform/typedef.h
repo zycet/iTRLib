43c80fe880239d0ccbe7d48b1e24984caa06ae58
张以成 <zycet@126.com>
2013-09-05 20:33:41 +0800

    添加基础类型定义

--- itrbase/platform/typedef.h
+++ itrbase/platform/typedef.h
@@ -2,15 +2,15 @@
 #define ITRBASE_PLATFORM_TYPEDEF_H_
 
 //为了方便移植并明确数据长度定义基础数据类型的别名
-typedef	unsigned char U8;
-typedef	signed char S8;
-typedef	unsigned int U32;
-typedef	signed int S32;
-typedef	float F32;
-typedef	double F64;
-typedef	unsigned short U16;
-typedef	signed short S16;
-typedef	unsigned long long U64;
-typedef	signed long long S64;
+typedef unsigned char U8;
+typedef signed char S8;
+typedef unsigned int U32;
+typedef signed int S32;
+typedef float F32;
+typedef double F64;
+typedef unsigned short U16;
+typedef signed short S16;
+typedef unsigned long long U64;
+typedef signed long long S64;
 
 #endif
