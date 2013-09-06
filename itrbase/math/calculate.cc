6d71e3ba14b98f4d7e6cb384ce817d27e1aff90d
Ethan-Zhou <cavatina@yeah.net>
2013-09-06 21:53:49 +0800

    完成calculate:批量计算，除了归一化，因为缺少Sqrt函数

--- itrbase/math/calculate.cc
+++ itrbase/math/calculate.cc
@@ -17,8 +17,7 @@ namespace itr_math
     {
     }
 
-    void Calculate::Add(S32* SourceA,S32* SourceB, S32 Length,
-            S32* Result)
+    void Calculate::Add(S32* SourceA, S32* SourceB, S32 Length, S32* Result)
     {
         for (S32 i = 0; i < Length; i++)
         {
@@ -26,8 +25,7 @@ namespace itr_math
         }
     }
 
-    void Calculate::Add(F32* SourceA,F32* SourceB, S32 Length,
-            F32* Result)
+    void Calculate::Add(F32* SourceA, F32* SourceB, S32 Length, F32* Result)
     {
         for (S32 i = 0; i < Length; i++)
         {
@@ -35,22 +33,175 @@ namespace itr_math
         }
     }
 
-    void Calculate::Sub(S32* SourceA,S32* SourceB,S32 Length,
-            S32* Result)
+    void Calculate::Sub(S32* SourceA, S32* SourceB, S32 Length, S32* Result)
     {
         for (S32 i = 0; i < Length; i++)
-                {
-                    Result[i] = SourceA[i] - SourceB[i];
-                }
+        {
+            Result[i] = SourceA[i] - SourceB[i];
+        }
     }
 
-    void Calculate::Sub(F32* SourceA,F32* SourceB, S32 Length,
-                F32* Result)
+    void Calculate::Sub(F32* SourceA, F32* SourceB, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
         {
-            for (S32 i = 0; i < Length; i++)
-            {
-                Result[i] = SourceA[i] - SourceB[i];
-            }
+            Result[i] = SourceA[i] - SourceB[i];
         }
+    }
 
+    void Calculate::Multi(S32* SourceA, S32* SourceB, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] * SourceB[i];
+        }
+    }
+
+    void Calculate::Multi(F32* SourceA, F32* SourceB, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] * SourceB[i];
+        }
+    }
+
+    void Calculate::Div(S32* SourceA, S32* SourceB, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] / SourceB[i];
+        }
+    }
+
+    void Calculate::Div(F32* SourceA, F32* SourceB, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] / SourceB[i];
+        }
+    }
+
+    void Calculate::Offset(S32* SourceA, S32 Offset, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] + Offset;
+        }
+    }
+
+    void Calculate::Offset(F32* SourceA, F32 Offset, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] + Offset;
+        }
+    }
+
+    void Calculate::Scale(S32* SourceA, S32 Scale, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] * Scale;
+        }
+    }
+
+    void Calculate::Scale(F32* SourceA, F32 Scale, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = SourceA[i] * Scale;
+        }
+    }
+
+    void Calculate::Normal(S32* SourceA, S32 Length, S32* Result)
+    {
+        S32 temp = 0;
+        for (S32 i = 0; i < Length; i++)
+        {
+            temp += SourceA[i] * SourceA[i];
+        }
+        for (S32 j = 0; j < Length; j++)
+        {
+            //Result[j] = SourceA[j]/
+            //TODO:暂时没有sqrt
+        }
+    }
+
+    void Calculate::Normal(F32* SourceA, S32 Length, F32* Result)
+    {
+        F32 temp = 0;
+        for (S32 i = 0; i < Length; i++)
+        {
+            temp += SourceA[i] * SourceA[i];
+        }
+        for (S32 j = 0; j < Length; j++)
+        {
+            //Result[j] = SourceA[j]/
+            //TODO:暂时没有sqrt
+        }
+    }
+
+    void Calculate::Abs(S32* SourceA, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            (SourceA[i] >= 0) ? Result[i] = SourceA[i] : Result[i] = -1 * SourceA[i];
+
+        }
+    }
+
+    void Calculate::Abs(F32* SourceA, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            (SourceA[i] >= 0) ? Result[i] = SourceA[i] : Result[i] = -1 * SourceA[i];
+        }
+    }
+
+    void Calculate::Opposite(S32* SourceA, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = -1 * SourceA[i];
+        }
+    }
+
+    void Calculate::Opposite(F32* SourceA, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[i] = -1 * SourceA[i];
+        }
+    }
+
+    void Calculate::Sum(S32* SourceA, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[0] += SourceA[i];
+        }
+    }
+
+    void Calculate::Sum(F32* SourceA, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[0] += SourceA[i];
+        }
+    }
+    void Calculate::Product(S32* SourceA, S32 Length, S32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[0] *= SourceA[i];
+        }
+    }
+
+    void Calculate::Product(F32* SourceA, S32 Length, F32* Result)
+    {
+        for (S32 i = 0; i < Length; i++)
+        {
+            Result[0] *= SourceA[i];
+        }
+    }
 } /* namespace itr_math */
