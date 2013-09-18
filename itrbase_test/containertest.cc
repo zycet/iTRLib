17d9571ef8cd6f6ca53c840679fdbfede82f2535
张以成 <zycet@126.com>
2013-09-18 20:37:16 +0800

    按要求增加测试函数

--- itrbase_test/containertest.cc
+++ itrbase_test/containertest.cc
@@ -11,25 +11,32 @@
 void TestCycleQueue()
 {
     S32 t;
-    itr_container::CycleQueue<S32> cycleQueue(4);
+    itr_container::CycleQueue<S32> cycleQueue(5);
     cycleQueue.Insert(1);
     cycleQueue.Insert(2);
     cycleQueue.Insert(3);
     cycleQueue.Insert(4);
     cycleQueue.Insert(5);
     cycleQueue.Insert(6);
-    assert(cycleQueue[0] == 3);
-    assert(cycleQueue[1] == 4);
-    assert(cycleQueue[2] == 5);
-    assert(cycleQueue[3] == 6);
-    cycleQueue.Fetch(t);
-    assert(t == 3);
-    cycleQueue.Fetch(t);
-    assert(t == 4);
-    cycleQueue.Fetch(t);
-    assert(t == 5);
+    cycleQueue.Insert(7);
+    cycleQueue.Insert(8);
+    cycleQueue.Insert(9);
+    cycleQueue.Insert(10);
+    assert(cycleQueue[0] == 6);
+    assert(cycleQueue[1] == 7);
+    assert(cycleQueue[2] == 8);
+    assert(cycleQueue[3] == 9);
+    assert(cycleQueue[4] == 10);
     cycleQueue.Fetch(t);
     assert(t == 6);
+    cycleQueue.Fetch(t);
+    assert(t == 7);
+    cycleQueue.Fetch(t);
+    assert(t == 8);
+    cycleQueue.Fetch(t);
+    assert(t == 9);
+    cycleQueue.Fetch(t);
+    assert(t == 10);
     assert(cycleQueue.Fetch(t) == false);
 
     TRACE_INFO("OK TestCycleQueue()");
