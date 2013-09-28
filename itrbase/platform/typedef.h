d44f3e6a6ce8a62a83edd554260a2f71f065eef2
ghdawn <ghdawn31@gmail.com>
2013-09-28 13:22:35 +0800

    修改了有符号的定义 修复了输出

--- itrbase/platform/typedef.h
+++ itrbase/platform/typedef.h
@@ -3,14 +3,14 @@
 
 //涓轰簡鏂逛究绉绘骞舵槑纭暟鎹暱搴﹀畾涔夊熀纭�暟鎹被鍨嬬殑鍒悕
 typedef unsigned char U8;
-typedef signed char S8;
+typedef char S8;
 typedef unsigned short U16;
-typedef signed short S16;
+typedef short S16;
 typedef unsigned int U32;
-typedef signed int S32;
+typedef int S32;
 typedef float F32;
 typedef unsigned long long U64;
-typedef signed long long S64;
+typedef long long S64;
 typedef double F64;
 typedef unsigned char BOOL;
 
