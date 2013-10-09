979d59642c84b855696cf6f96ccf376881129005
Ethan-Zhou <cavatina@yeah.net>
2013-10-09 19:38:30 +0800

    整理代码，将无用到调试代码删除

--- itrbase_test/mathtest.cc
+++ itrbase_test/mathtest.cc
@@ -344,11 +344,11 @@ void TestMatrix()
 
     Source1.Inv(Result);
     Result = Source1 * Result;
-    assert(fabs(Result.GetData()[0] - 1) < 0.001 && fabs(Result.GetData()[4] - 1) < 0.001 && fabs(Result.GetData()[8] - 1) < 0.001);
+    assert(fabs(Result.GetData()[0] - 1) < 0.0001 && fabs(Result.GetData()[4] - 1) < 0.0001 && fabs(Result.GetData()[8] - 1) < 0.0001);
     Result.Set(0);
 
     Source1.Tran(Result);
-    assert(Source1.GetData()[0] == Result.GetData()[0]);// && Source1.GetData()[1] == Result.GetData()[3] && Source1.GetData()[2] == Result.GetData()[6]);
+    assert(Source1.GetData()[0] == Result.GetData()[0] && Source1.GetData()[1] == Result.GetData()[3] && Source1.GetData()[2] == Result.GetData()[6]);
 
     TRACE_INFO("OK TestMatrix()");
 }
