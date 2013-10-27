41fa5fd80b0d3675e289eb5cec73680a89b6114c
张以成 <zycet@126.com>
2013-10-27 10:57:15 +0800

    drawtest再修改

--- itrvision_test/drawtest.cc
+++ itrvision_test/drawtest.cc
@@ -8,15 +8,15 @@
 void drawtest()
 {
     drawImgGraytestCircle();
-    void drawImgARGBtestCircle();
-    void drawImgGraytestLineOffset();
-    void drawImgARGBtestLineOffset();
-    void drawImgGraytestLine();
-    void drawImgARGBtestLine();
-    void drawImgGraytestCross();
-    void drawImgARGBtestCross();
-    void drawImgGraytestRectangle();
-    void drawImgARGBtestRectangle();
+    drawImgARGBtestCircle();
+    drawImgGraytestLineOffset();
+    drawImgARGBtestLineOffset();
+    drawImgGraytestLine();
+    drawImgARGBtestLine();
+    drawImgGraytestCross();
+    drawImgARGBtestCross();
+    drawImgGraytestRectangle();
+    drawImgARGBtestRectangle();
 }
 void drawImgGraytestCircle()
 {
@@ -25,55 +25,55 @@ void drawImgGraytestCircle()
     Draw::Circle(gray, 100, 80, 10, 255);
     IOHelper::WritePGMFile("Debug/gray1.pgm", gray);
 }
-/*
+
 void drawImgARGBtestCircle()
 {
     ImageGray ARGB;
-    IOHelper::ReadPGMFile("Debug/draw/cap000.pgm", ARGB);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", ARGB);
     Draw::Circle(ARGB, 100, 80, 10, 255);
-    IOHelper::WritePGMFile(file, ARGB);
+    IOHelper::WritePGMFile("Debug/gray2.pgm", ARGB);
 }
 void drawImgGraytestLineOffset()
 {
     ImageGray gray;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", gray);
     Draw::LineOffset(gray, 100, 80, 200, 160, 255);
-    IOHelper::WritePGMFile(file, gray);
+    IOHelper::WritePGMFile("Debug/gray3.pgm", gray);
 }
 void drawImgARGBtestLineOffset()
 {
     ImageGray ARGB;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", ARGB);
     Draw::LineOffset(ARGB, 100, 80, 200, 160, 255);
-    IOHelper::WritePGMFile(file, ARGB);
+    IOHelper::WritePGMFile("Debug/gray4.pgm", ARGB);
 }
 void drawImgGraytestLine()
 {
     ImageGray gray;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", gray);
     Draw::Line(gray, 1600, 1800, 100, 120, 255);
-    IOHelper::WritePGMFile(file, gray);
+    IOHelper::WritePGMFile("Debug/gray5.pgm", gray);
 }
 void drawImgARGBtestCross()
 {
     ImageGray ARGB;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", ARGB);
     Draw::Line(ARGB, 800, 600, 100, 120, 255);
-    IOHelper::WritePGMFile(file, ARGB);
+    IOHelper::WritePGMFile("Debug/gray6.pgm", ARGB);
 }
 void drawImgGraytestRectangle()
 {
     RectangleS rect(210, 240, 100, 150);
     ImageGray gray;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", gray);
     Draw::Rectangle(gray, rect, 255);
-    IOHelper::WritePGMFile(file, gray);
+    IOHelper::WritePGMFile("Debug/gray7.pgm", gray);
 }
 void drawImgARGBtestRectangle()
 {
     RectangleS rect(410, 440, 100, 150);
     ImageGray ARGB;
-    IOHelper::ReadPGMFile("Debug/green/cap000.pgm", gray);
+    IOHelper::ReadPGMFile("Debug/img0.pgm", ARGB);
     Draw::Rectangle(ARGB, rect, 255);
-    IOHelper::WritePGMFile(file, ARGB);
-}*/
+    IOHelper::WritePGMFile("Debug/gray8.pgm", ARGB);
+}
