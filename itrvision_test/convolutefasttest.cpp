870a6547b3843fbecbe6d9246aff1231102954dd
张以成 <zycet@126.com>
2014-01-22 10:33:03 +0800

    Test

--- itrvision_test/convolutefasttest.cpp
+++ itrvision_test/convolutefasttest.cpp
@@ -6,14 +6,15 @@
 #include <iostream>
 #include <sys/time.h>
 
-using namespace std;
-using namespace itr_math;
 
-#define TEST_CYC 100
+
+#define TEST_CYC 50
 
 #define filtervalue 0.33333
 #define  filterN 71
-void TestCompare()
+//using namespace std;
+//using namespace itr_math;
+void TestComparefast()
 {
     //Time
     struct timeval tpstart;
@@ -37,20 +38,56 @@ void TestCompare()
     }
     //Calc
     gettimeofday(&tpstart,NULL);
+    ConvoluteFast con;
+    con.Init( filterN, grayI.GetWidth(),grayI.GetHeight());
     for(S32 i=0;i<TEST_CYC;i++)
     {
-        itr_math::CalculateObj->Compare(a, b, err, TEST_NUM,&IsSame);
-
-        ConvoluteFast con;
-        con.Init( filterN, grayI.GetWidth(),grayI.GetHeight());
         con.Convolute(gray_matrix_in, Filter, graym_matrix_out);
-
-        ConvoluteSquare conv;
-        conv._KLTComputeSmoothedImage(Img, 0.7, img[0]);//Input sigma Output
     }
     gettimeofday(&tpend,NULL);
     timeuse = 1000000*(tpend.tv_sec-tpstart.tv_sec)+tpend.tv_usec-tpstart.tv_usec;
     timeuse /= 1000;
+    printf("\n fast : %f\n",timeuse);
+    TRACE_DEBUG(timeuse);
+    //Verify
+
+}
+void TestComparesquare()
+{
+    //Time
+    struct timeval tpstart;
+    struct timeval tpend;
+    F32 timeuse = 0 ;
+    //Data
+    ImageGray grayI;
+    IOHelper::ReadPGMFile("table1.pgm", grayI);
+    ImageGray grayO(grayI.GetWidth(),grayI.GetHeight());
+
+    Matrix gray_matrix_in(grayI.GetWidth(),grayI.GetHeight());
+    Matrix graym_matrix_out(grayI.GetWidth(),grayI.GetHeight());
+
+    ConvertFormat::ImageGray2Matrix(grayI,gray_matrix_in);
+
+    //Init
+    F32 Filter[filterN];
+    for(S32 i=0; i<filterN; i++)
+    {
+        *(Filter+i)=filtervalue;
+    }
+    //Calc
+    gettimeofday(&tpstart,NULL);
+    ConvoluteSquare conv;
+    for(S32 i=0;i<TEST_CYC;i++)
+    {
+        //itr_math::CalculateObj->Compare(a, b, err, TEST_NUM,&IsSame);
+
+        conv._KLTComputeSmoothedImage(gray_matrix_in, 0.7, graym_matrix_out);//Input sigma Output
+    }
+    gettimeofday(&tpend,NULL);
+    timeuse = 1000000*(tpend.tv_sec-tpstart.tv_sec)+tpend.tv_usec-tpstart.tv_usec;
+    timeuse /= 1000;
+
+    printf("\n square : %f\n",timeuse);
     TRACE_DEBUG(timeuse);
     //Verify
 
@@ -60,14 +97,14 @@ void ConvoluteFastTest()
 {
 
 
-    PRINT_DEBUG("!!!Neon!!!");
-    itr_math::MathNeonObjStandInit();
-    TestCompare();
-    itr_math::MathNeonObjStandDeinit();
-    PRINT_DEBUG("!!!C++!!!");
-    itr_math::MathObjStandInit();
-    TestCompare();
-    itr_math::MathObjStandDeinit();
+    PRINT_DEBUG("!!!fast!!!");
+   // itr_math::MathNeonObjStandInit();
+    TestComparefast();
+    //itr_math::MathNeonObjStandDeinit();
+    PRINT_DEBUG("!!!square!!!");
+   // itr_math::MathObjStandInit();
+    TestComparesquare();
+   // itr_math::MathObjStandDeinit();
 
 
 
