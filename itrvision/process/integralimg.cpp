d17eeec620eff98cab3204a84af6d3e18d34bbf0
张以成 <zycet@126.com>
2014-02-01 21:13:46 +0800

    积分图像

--- itrvision/process/integralimg.cpp
+++ itrvision/process/integralimg.cpp
@@ -5,7 +5,15 @@ void IntegralImg::Normal(const Matrix &input,Matrix &output)
     S32 row = input.GetRow();
     S32 col = input.GetCol();
     F32 *Data = input.GetData();
+    F32 result = 0.0f;
 
+    for(S32 j=0; j<row; j++)
+    {
+        for(S32 i=0; i <col; i++)
+        {
+            result += Data[i + j*row];
+        }
+    }
 
 }
 
@@ -14,4 +22,11 @@ void IntegralImg::Square(const Matrix &input,Matrix &output)
     S32 row = input.GetRow();
     S32 col = input.GetCol();
     F32 *Data = input.GetData();
+    for(S32 j=0; j<row; j++)
+    {
+        for(S32 i=0; i <col; i++)
+        {
+            result += Data[i + j*row] * Data[i + j*row];
+        }
+    }
 }
