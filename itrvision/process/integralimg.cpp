03ff380eaa8ab45a421feff5a5d05cbbc87da49b
张以成 <zycet@126.com>
2014-02-01 21:57:05 +0800

    大幅修改nc错误

--- itrvision/process/integralimg.cpp
+++ itrvision/process/integralimg.cpp
@@ -4,14 +4,22 @@ void IntegralImg::Normal(const Matrix &input,Matrix &output)
 {
     S32 row = input.GetRow();
     S32 col = input.GetCol();
-    F32 *Data = input.GetData();
+    F32 *IData = input.GetData();
+    F32 *OData = output.GetData();
     F32 result = 0.0f;
 
-    for(S32 j=0; j<row; j++)
+    for(S32 j=0; j <col; j++)
     {
-        for(S32 i=0; i <col; i++)
+        result += IData[j];
+        OData[j] = result;
+    }
+    for(S32 i=1; i<row; i++)
+    {
+        result = 0.0f;
+        for(S32 j=0; j<col; j++)
         {
-            result += Data[i + j*row];
+            result += Data[j + i*row];
+            OData[j + i*row] = result + OData[(i-1)*step+j];
         }
     }
 
@@ -22,11 +30,20 @@ void IntegralImg::Square(const Matrix &input,Matrix &output)
     S32 row = input.GetRow();
     S32 col = input.GetCol();
     F32 *Data = input.GetData();
-    for(S32 j=0; j<row; j++)
+    F32 *OData = output.GetData();
+    F32 result = 0.0f;
+
+    for(S32 j=0; j<col; j++)
+    {
+        result += IData[j]*IData[j];
+        OData[j] = result;
+    }
+    for(S32 i=1; i <row; i++)
     {
         for(S32 i=0; i <col; i++)
         {
             result += Data[i + j*row] * Data[i + j*row];
+            OData[j + i*row] = result + OData[(i-1)*step+j];
         }
     }
 }
