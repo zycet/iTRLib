71a1358c0dce7181297851b28f1dcfdaa79ac8ce
张以成 <zycet@126.com>
2014-02-01 11:28:43 +0800

    特征点commfeaturepoint.cpp

--- itrvision/feature/commfeaturepoint.cpp
+++ itrvision/feature/commfeaturepoint.cpp
@@ -9,8 +9,21 @@ namespace itr_vision
 CommFeaturePoint::CommFeaturePoint():Point2D()
 {
     //ctor
+    ID =0;
+    Tag =NULL;
+    Dir =0;
+    Quality=0;
+    Value=0;
 }
+/*
+void CommFeaturePoint::Init(Point2D Pos,S32 ID,F32 Dir,F32 Quality,F32 Value)
+{
 
+}
+void CommFeaturePoint::Init(Point2D Pos,F32 Quality,F32 Value)
+{
+
+}*/
 CommFeaturePoint::~CommFeaturePoint()
 {
     //dtor
@@ -19,6 +32,11 @@ CommFeaturePoint::~CommFeaturePoint()
 CommFeaturePoint::CommFeaturePoint(const CommFeaturePoint& other)
 {
     //copy ctor
+    ID =other.ID;
+    Tag =other.Tag;
+    Dir =other.Dir;
+    Quality=other.Quality;
+    Value=other.Value;
 }
 
 }
