2d9db33115390d5c07b3ed040a9af45db35e215c
张以成 <zycet@126.com>
2014-02-01 21:12:11 +0800

    写了一部分积分图像，目测有一堆问题。。。

--- itrvision/process/integralimg.cpp
+++ itrvision/process/integralimg.cpp
@@ -2,10 +2,16 @@
 
 void IntegralImg::Normal(const Matrix &input,Matrix &output)
 {
+    S32 row = input.GetRow();
+    S32 col = input.GetCol();
+    F32 *Data = input.GetData();
+
 
 }
 
 void IntegralImg::Square(const Matrix &input,Matrix &output)
 {
-
+    S32 row = input.GetRow();
+    S32 col = input.GetCol();
+    F32 *Data = input.GetData();
 }
