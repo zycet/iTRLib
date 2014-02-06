54503aa4ea3e420e23412d7cca8ce95d4af85e3d
张以成 <zycet@126.com>
2014-02-06 11:45:20 +0800

    特征点匹配告一段落

--- itrvision/feature/FeatureMatch.cpp
+++ itrvision/feature/FeatureMatch.cpp
@@ -15,15 +15,20 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
             VectorFeaturePoint vectorfeaturepoint2[],S32 num2,S32 matchnum,Point2D pos1[],Point2D pos2[])
 {
 
-    //dis_Ratio = 0.6;//最优匹配和次优匹配的比例
-    F32 Max_ang = 18;   //匹配极限偏差，角度制
-    F32 valuet1[num2];
-    F32 valuet2[num1];
-    F32 result_min,result_ang;
+    F32 valuet1[num2];  //存储向量点积
+    F32 valuet2[num1];  //存储向量点积，两者不冲突，可以用valuet[max(num2,num1)]代替，但是我不会，暂时先浪费点空间
+    F32 result_ang;
+    S32 order;
     bool match1[num1];
+    S32 match1order[num1];
     bool match2[num2];
+    S32 match2order[num2];
     Calculate CalculateObj;
     Numerical NumericalObj;
+
+    //dis_Ratio = 0.6;//最优匹配和次优匹配的比例
+    F32 Max_ang;
+    NumericalObj.Cos(18,Max_ang);   //匹配极限偏差，角度制
     //第一副图向第二副图匹配
     for(S32 i=0; i<num1; i++)
     {
@@ -32,10 +37,11 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
         {
             valuet1[j] = vectorfeaturepoint1[i].Feature.ProductInner(vectorfeaturepoint2[j].Feature);
         }
-        CalculateObj.Min(valuet1, num2, result_min);//需要索引啊！即最小值对应的编号。
-        NumericalObj.Acos(result_min,result_ang);
+        CalculateObj.Min(valuet1, num2, result_ang,order);
         if(result_ang<Max_ang)
-            match1[i]=true;
+        {    match1[i]=true;
+            match1order[i]=order;
+        }
         else
             match1[i]=false;
         if(i>0)
@@ -51,10 +57,11 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
         {
             valuet2[j] = vectorfeaturepoint2[i].Feature.ProductInner(vectorfeaturepoint1[j].Feature);
         }
-        CalculateObj.Min(valuet2, num1, result_min);//需要索引啊！即最小值对应的编号。
-        NumericalObj.Acos(result_min,result_ang);
+        CalculateObj.Min(valuet2, num1, result_ang,order);
         if(result_ang<Max_ang)
-            match2[i]=true;
+        {   match2[i]=true;
+            match2order[i]=order;
+        }
         else
             match2[i]=false;
         if(i>0)
@@ -62,9 +69,26 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
                 if(match2[i]&&vectorfeaturepoint2[i].X==vectorfeaturepoint2[j].X&&vectorfeaturepoint2[i].Y==vectorfeaturepoint2[j].Y)
                     match2[i]=false;
     }
+
     //双向匹配
-
+    matchnum =0;
+    for(S32 i=0; i<num1; i++)
+    {
+        if(match1[i]&&match2[match1order[i]]&&match2order[match1order[i]]==i)
+        {
+            matchnum++;
+        }
+        else
+            match1[i]=false;
+    }
     //匹配结果
-
+    for(S32 i=0; i<num1; i++)
+    {
+        if(match1[i])
+        {
+            pos1->SetXY(vectorfeaturepoint1[i].X,vectorfeaturepoint1[i].Y);
+            pos2->SetXY(vectorfeaturepoint2[match1order[i]].X,vectorfeaturepoint2[match1order[i]].Y);
+        }
+    }
 }
 }
