fa153d671301a5fb70f10c429d1fce7faaffc608
张以成 <zycet@126.com>
2014-02-06 11:54:55 +0800

    特征点匹配完成，等待学长指正。

--- itrvision/feature/FeatureMatch.cpp
+++ itrvision/feature/FeatureMatch.cpp
@@ -16,7 +16,7 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
 {
 
     F32 valuet1[num2];  //存储向量点积
-    F32 valuet2[num1];  //存储向量点积，两者不冲突，可以用valuet[max(num2,num1)]代替，但是我不会，暂时先浪费点空间
+    F32 valuet2[num1];  //存储向量点积，两者不冲突，可以用valuet[max(num2,num1)]代替，但是我不会，暂时先浪费点空间？？？？？？？？？
     F32 result_ang;
     S32 order;
     bool match1[num1];
@@ -26,9 +26,9 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
     Calculate CalculateObj;
     Numerical NumericalObj;
 
-    //dis_Ratio = 0.6;//最优匹配和次优匹配的比例
-    F32 Max_ang;
-    NumericalObj.Cos(18,Max_ang);   //匹配极限偏差，角度制
+    //dis_Ratio = 0.6;//最优匹配和次优匹配的比例,由于calculate函数中暂无求次小值函数，暂时不实现。？？？？？？？？？？？？？？？？？？
+    F32 Max_ang;    //匹配极限偏差，角度的余弦值
+    NumericalObj.Cos(18,Max_ang);
     //第一副图向第二副图匹配
     for(S32 i=0; i<num1; i++)
     {
@@ -82,12 +82,14 @@ void FeatureMatch::FeatureMatchDo(VectorFeaturePoint vectorfeaturepoint1[],S32 n
             match1[i]=false;
     }
     //匹配结果
+    S32 tempcounter =0;
     for(S32 i=0; i<num1; i++)
     {
         if(match1[i])
         {
-            pos1->SetXY(vectorfeaturepoint1[i].X,vectorfeaturepoint1[i].Y);
-            pos2->SetXY(vectorfeaturepoint2[match1order[i]].X,vectorfeaturepoint2[match1order[i]].Y);
+            pos1[tempcounter].SetXY(vectorfeaturepoint1[i].X, vectorfeaturepoint1[i].Y);
+            pos2[tempcounter].SetXY(vectorfeaturepoint2[match1order[i]].X, vectorfeaturepoint2[match1order[i]].Y);
+            tempcounter++;
         }
     }
 }
