8e0fe141131391c2eaab9def8fb494f55cb28868
张以成 <zycet@126.com>
2014-02-08 17:28:03 +0800

    fasthessian 依旧存在问题。。。

--- itrvision/process/fasthessian.h
+++ itrvision/process/fasthessian.h
@@ -2,23 +2,20 @@
 #define FASTHESSIAN_H
 #include "itrbase.h"
 #include "./feature.h"
+#include <vector>
 using itr_math::Matrix;
 class FastHessian
 {
     public:
         FastHessian(Matrix &img,
                     std::vector<Ipoint> &ipts,
-                    const int octaves = 5,
-                    const int intervals = 4,
-                    const int init_sample = 8,
-                    const float thres = 0.0004f);
+                    const int octaves ,
+                    const int intervals ,
+                    const int init_sample ,
+                    const float thres );
 
          ~FastHessian();
-         void saveParameters(const int octaves,
-                             const int intervals,
-                             const int init_sample,
-                             const float thres);
-         void setIntImage(Matrix &img);
+
          void getIpoints();
     protected:
     private:
