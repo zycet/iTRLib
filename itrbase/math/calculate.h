1d4b169612488c2431d39643189d89a47278a52c
张以成 <zycet@126.com>
2014-02-17 21:23:21 +0800

    完成calculate的注释

--- itrbase/math/calculate.h
+++ itrbase/math/calculate.h
@@ -1,35 +1,3 @@
-/*
- *
- *   Copyright (C) 2013 BUAA iTR Research Center. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * calculate.cc
- *  Created on: 2013-9-9
- *      Author: ZYC
- */
 
 #ifndef CALCULATE_H_
 #define CALCULATE_H_
@@ -44,58 +12,137 @@ class Calculate
 public:
     Calculate();
     virtual ~Calculate();
+    /**
+    * \brief 两数组元素对应相加
+    * \param SourceA 数组Ａ
+    * \param SourceB 数组Ｂ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Add(S16* SourceA, S16* SourceB, S32 Length, S16* Result) const;
     virtual void Add(S32* SourceA, S32* SourceB, S32 Length, S32* Result) const;
     virtual void Add(F32* SourceA, F32* SourceB, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 两数组元素对应相减
+    * \param SourceA 数组Ａ
+    * \param SourceB 数组Ｂ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Sub(S16* SourceA, S16* SourceB, S32 Length, S16* Result) const;
     virtual void Sub(S32* SourceA, S32* SourceB, S32 Length, S32* Result) const;
     virtual void Sub(F32* SourceA, F32* SourceB, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 两数组元素对应相乘
+    * \param SourceA 数组Ａ
+    * \param SourceB 数组Ｂ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Multi(S16* SourceA, S16* SourceB, S32 Length, S16* Result) const;
     virtual void Multi(S32* SourceA, S32* SourceB, S32 Length, S32* Result) const;
     virtual void Multi(F32* SourceA, F32* SourceB, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 两数组元素对应相除
+    * \param SourceA 数组Ａ
+    * \param SourceB 数组Ｂ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Div(S16* SourceA, S16* SourceB, S32 Length, S16* Result) const;
     virtual void Div(S32* SourceA, S32* SourceB, S32 Length, S32* Result) const;
     virtual void Div(F32* SourceA, F32* SourceB, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 数组中所有元素右移DivOrder位
+    * \param SourceA 数组Ａ
+    * \param DivOrder 右移位数
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void DivOrder(S16* SourceA, S32 DivOrder, S32 Length, S16* Result) const;
     virtual void DivOrder(S32* SourceA, S32 DivOrder, S32 Length, S32* Result) const;
-
+    /**
+    * \brief 数组中所有元素加Offset
+    * \param SourceA 数组Ａ
+    * \param Offset 加数
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Offset(S16* SourceA, S16 Offset, S32 Length, S16* Result) const;
     virtual void Offset(S32* SourceA, S32 Offset, S32 Length, S32* Result) const;
     virtual void Offset(F32* SourceA, F32 Offset, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 数组中所有元素乘Scale
+    * \param SourceA 数组Ａ
+    * \param Scale 乘数
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Scale(S16* SourceA, S16 Scale, S32 Length, S16* Result) const;
     virtual void Scale(S32* SourceA, S32 Scale, S32 Length, S32* Result) const;
     virtual void Scale(F32* SourceA, F32 Scale, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 归一化
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Normalization(S16* SourceA, S32 Length, S16* Result) const;
     virtual void Normalization(S32* SourceA, S32 Length, S32* Result) const;
     virtual void Normalization(F32* SourceA, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 求数组中所有元素的绝对值
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Abs(S16* SourceA, S32 Length, S16* Result) const;
     virtual void Abs(S32* SourceA, S32 Length, S32* Result) const;
     virtual void Abs(F32* SourceA, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 求数组中所有元素的相反数
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Opposite(S16* SourceA, S32 Length, S16* Result) const;
     virtual void Opposite(S32* SourceA, S32 Length, S32* Result) const;
     virtual void Opposite(F32* SourceA, S32 Length, F32* Result) const;
-
+    /**
+    * \brief 将数组中所有元素求和
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void AddSum(S16* SourceA, S32 Length, S16& Result) const;
     virtual void AddSum(S32* SourceA, S32 Length, S32& Result) const;
     virtual void AddSum(F32* SourceA, S32 Length, F32& Result) const;
-
+    /**
+    * \brief 将两个数组中对应元素元素相称并求和
+    * \param SourceA 数组Ａ
+    * \param SourceA 数组Ｂ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void MultiSum(S16* SourceA, S16* SourceB, S32 Length, S16& Result) const;
     virtual void MultiSum(S16* SourceA, F32* SourceB, S32 Length, S16& Result) const;
     virtual void MultiSum(S32* SourceA, S32* SourceB, S32 Length, S32& Result) const;
     virtual void MultiSum(F32* SourceA, F32* SourceB, S32 Length, F32& Result) const;
-
+    /**
+    * \brief 求数组中所有元素的连乘积
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Result 输出结果
+    */
     virtual void Product(S16* SourceA, S32 Length, S16& Result) const;
     virtual void Product(S32* SourceA, S32 Length, S32& Result) const;
     virtual void Product(F32* SourceA, S32 Length, F32& Result) const;
-
+    /**
+    * \brief 将数组中所有元素统一赋值
+    * \param SourceA 数组Ａ
+    * \param Length 数组长度
+    * \param Value 所赋值
+    */
     virtual void Set(S16* SourceA, S16 Value, S32 Length) const;
     virtual void Set(S32* SourceA, S32 Value, S32 Length) const;
     virtual void Set(F32* SourceA, F32 Value, S32 Length) const;
@@ -124,6 +171,7 @@ public:
       * \brief 求两个向量的一次距离
       * \param SourceA 向量1
       * \param SourceB 向量2
+      * \param Length 向量维数
       * \param Result 向量1，2的一次距离和
       */
     virtual void Single_dis(F32* SourceA, F32* SourceB, S32 Length, F32& Result) const;
@@ -131,10 +179,18 @@ public:
       * \brief 求两个向量的二次距离
       * \param SourceA 向量1
       * \param SourceB 向量2
+      * \param Length 向量维数
       * \param Result 向量1，2的二次距离和
       */
     virtual void Double_dis(F32* SourceA, F32* SourceB, S32 Length, F32& Result) const;
-
+    /**
+      * \brief 判断两组向量是否相同
+      * \param SourceA 向量1
+      * \param SourceB 向量2
+      * \param Length 向量维数
+      * \param Error 阈值
+      * \param IsSame 布尔输出两向量是否相同，0为相同1为不同
+      */
     virtual void Compare(F32* SourceA, F32* SourceB, F32 Error,S32 Length,BOOL* IsSame) const;
 
 };
