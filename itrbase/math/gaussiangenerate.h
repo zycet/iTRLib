894547de68e48715a92071b8ebe95c57a8462a1f
张以成 <zycet@126.com>
2014-02-20 16:52:24 +0800

    完善base注释

--- itrbase/math/gaussiangenerate.h
+++ itrbase/math/gaussiangenerate.h
@@ -12,13 +12,13 @@ class GaussianGenerate
 public:
     static void Generate(F32 Sigma, S32 Length, F32* Data);
     /**
-    * \brief 求高斯加权数组的建议长度
-    * \param Sigma 数组中个元素的和
+    * /brief 求高斯加权数组的建议长度
+    * /param Sigma 数组中个元素的和
     */
     static S32 SuggestLength(F32 Sigma);
     /**
-    * \brief 求高斯加权数组的建议长度
-    * \param Sigma 数组中个元素的和
+    * /brief 求高斯加权数组的建议长度
+    * /param Sigma 数组中个元素的和
     */
     static F32 PDF(F32 mu, F32 sigma, F32 value);
 };
