75a1655e2844b416760a9ce76fb6749de9d0b5ee
张以成 <zycet@126.com>
2014-02-28 19:43:56 +0800

    修改cameraexternal中对应变更部分

--- itrvision/camera/camerastereocalc.cpp
+++ itrvision/camera/camerastereocalc.cpp
@@ -5,12 +5,25 @@ namespace itr_vision
 
 CameraStereoCalc::CameraStereoCalc()
 {
-    //ctor
+    //(CalcExInfo.Equ).Init(3);//ctor
+    this->InstallAngle=0;
 }
 
 CameraStereoCalc::~CameraStereoCalc()
 {
     //dtor
 }
+void CameraStereoCalc::Init(CameraInterCalc* CameraInterCalc0,CameraInterCalc* CameraInterCalc1,F32 Distance)
+{
+    this->Distance=Distance;
+    this->CameraInterCalc0=CameraInterCalc0;
+    this->CameraInterCalc1=CameraInterCalc1;
+}
+
+
+//this->CalcExInfo CameraStereoCalc::CalcExInfo GetCalcExInfo()
+//{
+//
+//}
 
 }
