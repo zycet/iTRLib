9fd728fc33bb881fd82caa9975651dbba5556095
张以成 <zycet@126.com>
2014-03-06 10:29:34 +0800

    编写itrdevice/camera 修改了cyclequeue.h-增加后初始化函数 准备测试camera

--- itrdevice/itrdevice.h
+++ itrdevice/itrdevice.h
@@ -1,7 +1,8 @@
 #ifndef ITRDEVICE_H_INCLUDED
 #define ITRDEVICE_H_INCLUDED
 
-#include "./camera/Iimageaquaire.h"
+#include "Iimageaquaire.h"
+#include "stdimageaquaire.h"
 #include "Ijoystick.h"
 #include "stdjoystick.h"
 #endif // ITRDEVICE_H_INCLUDED
--- itrdevice_test/main.cpp
+++ itrdevice_test/main.cpp
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include "itrbase.h"
 #include "joysticktest.h"
+#include "stdcameratest.h"
 
+#include "itrdevice.h"
 int main()
 {
     JoyStickTest();
-
+    stdcamera_test();
     return 0;
 }
