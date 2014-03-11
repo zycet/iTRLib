c1a7e90b8a4cff6de401b7a74fe540cc2c1a4878
张以成 <zycet@126.com>
2014-03-11 21:46:46 +0800

    修改头文件

--- itrdevice/itrdevice.h
+++ itrdevice/itrdevice.h
@@ -1,8 +1,8 @@
 #ifndef ITRDEVICE_H_INCLUDED
 #define ITRDEVICE_H_INCLUDED
 
-#include "icamera.h"
-#include "asicamera.h"
-#include "Ijoystick.h"
-#include "stdjoystick.h"
+#include "camera/icamera.h"
+#include "camera/asicamera.h"
+#include "joystick/Ijoystick.h"
+#include "joystick/stdjoystick.h"
 #endif // ITRDEVICE_H_INCLUDED
