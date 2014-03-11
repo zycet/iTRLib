e90ef73688de29d372c91bf73317eaca2b5769d8
张以成 <zycet@126.com>
2014-03-11 15:43:38 +0800

    编写itrdevice v4l

---
+++ itrdevice/camera/v4linux.h
@@ -0,0 +1,29 @@
+#ifndef V4LINUX_H
+#define V4LINUX_H
+#include "itrbase.h"
+#include "asicamera.h"
+namespace itr_device
+{
+class v4linux:public ICamera
+{
+    public:
+        struct Picture
+        {
+            unsigned char *data[4];
+            int stride[4];
+        };
+        typedef struct Picture Picture;
+
+        v4linux();
+        virtual ~v4linux();
+
+        void *capture_open(const char *dev_name, int v_width, int v_height, RawFormat fmt);
+        int capture_get_picture (void *id, Picture *pic);
+        int capture_close (void *id);
+
+        int capture_get_output_ptr (void *id, unsigned char ***ptr, int **stride);
+    protected:
+    private:
+};
+}
+#endif // V4LINUX_H
