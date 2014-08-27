463285e8491ea73abdf08527020fb82675341d35
ghdawn <ghdawn31@gmail.com>
2014-08-27 11:51:42 +0800

    之前忘了命名空间

--- itralgorithm/classifier/fern.h
+++ itralgorithm/classifier/fern.h
@@ -3,30 +3,32 @@
 #include"itrbase.h"
 #include<vector>
 using std::vector;
-class Fern
+namespace itr_algorithm
 {
-    public:
-        /** Default constructor */
-        Fern();
-        /** Default destructor */
-        virtual ~Fern();
-        Fern(S32 NFeat,S32 NComp,S32 Range);
-        void Init(S32 NFeat,S32 NComp,S32 Range);
-        F32 Classify(S32 *X);
-        void Train(const itr_math::Matrix &img,bool flag);
-    protected:
-    private:
-        void update(const vector<S32> &X,bool flag);
+    class Fern
+    {
+        public:
+            /** Default constructor */
+            Fern();
+            /** Default destructor */
+            virtual ~Fern();
+            Fern(S32 NFeat,S32 NComp,S32 Range);
+            void Init(S32 NFeat,S32 NComp,S32 Range);
+            F32 Classify(S32 *X);
+            void Train(const itr_math::Matrix &img,bool flag);
+        protected:
+        private:
+            void update(const vector<S32> &X,bool flag);
 
-        vector< vector<S32> > comp;
-        vector<S32> X;
-        vector< vector<S32> > pos;
-        vector< vector<S32> > neg;
-        vector< vector<F32> > post;
-        int ncomp;
-        int nfeat;
+            vector< vector<S32> > comp;
+            vector<S32> X;
+            vector< vector<S32> > pos;
+            vector< vector<S32> > neg;
+            vector< vector<F32> > post;
+            int ncomp;
+            int nfeat;
 
 
-};
-
+    };
+}
 #endif // FERN_H
